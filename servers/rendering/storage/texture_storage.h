32a05a59a0b0fce63eab6fe0e818b367acc11ac8
huisedenanhai <winser@pku.edu.cn>
2022-08-17 13:57:30 +0800

    Expose API for converting ImageTexture RID to RenderingDevice texture RID; API for creating ImageTexture with custom usage bits

--- servers/rendering/storage/texture_storage.h
+++ servers/rendering/storage/texture_storage.h
@@ -32,6 +32,7 @@
 #define TEXTURE_STORAGE_H
 
 #include "servers/rendering_server.h"
+#include <stdint.h>
 
 class RendererTextureStorage {
 private:
@@ -70,6 +71,11 @@ public:
 	virtual void texture_2d_layered_initialize(RID p_texture, const Vector<Ref<Image>> &p_layers, RS::TextureLayeredType p_layered_type) = 0;
 	virtual void texture_3d_initialize(RID p_texture, Image::Format, int p_width, int p_height, int p_depth, bool p_mipmaps, const Vector<Ref<Image>> &p_data) = 0;
 	virtual void texture_proxy_initialize(RID p_texture, RID p_base) = 0; //all slices, then all the mipmaps, must be coherent
+
+	virtual void texture_2d_with_usage_initialize(RID p_texture, const Ref<Image> &p_image, uint32_t p_usage_bits) = 0;
+	virtual void texture_2d_layered_with_usage_initialize(RID p_texture, const Vector<Ref<Image>> &p_layers, RS::TextureLayeredType p_layered_type, uint32_t p_usage_bits) = 0;
+	virtual void texture_3d_with_usage_initialize(RID p_texture, Image::Format, int p_width, int p_height, int p_depth, bool p_mipmaps, const Vector<Ref<Image>> &p_data, uint32_t p_usage_bits) = 0;
+	virtual RID texture_get_rd_rid(RID p_texture) const = 0;
 
 	virtual void texture_2d_update(RID p_texture, const Ref<Image> &p_image, int p_layer = 0) = 0;
 	virtual void texture_3d_update(RID p_texture, const Vector<Ref<Image>> &p_data) = 0;
